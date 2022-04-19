3ff25e882a32010e685f3c855a36c79f229ba21e
ullasraj <57474473+ullasraj@users.noreply.github.com>
2022-04-19 10:43:17 +0530

    [Ullas] | Add. Setup

--- Game/src/Game/EntryPoint.h
+++ Game/src/Game/EntryPoint.h
@@ -3,15 +3,17 @@
 
 #ifndef GAME_PLATFORM_WINDOWS
 
-extern Game::Application* Game::CreateApplication();
+ extern Game::Application* Game::CreateApplication();
 
 int main(int argc ,char** argv)
 {
+	
 	printf("hello");
+
 	auto app = Game::CreateApplication();
 	app->Run();
 	
-	return 0;
+	
 }
 
 #endif // PLATFORM_WINDOWS
