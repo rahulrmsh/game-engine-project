3d5774d53698e6c5854a416b010e981057400fe5
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-05-03 16:58:37 +0530

    [Rahul] | Add. Event Handling System - Mouse/Key Input

--- GammaEngine/src/GammaEngine/Application.cpp
+++ GammaEngine/src/GammaEngine/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Log.h"
+#include "Events/ApplicationEvent.h"
 
 namespace GammaEngine {
 	Application::Application()
@@ -11,6 +13,16 @@ namespace GammaEngine {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			GAMMAENGINE_CORE_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			GAMMAENGINE_CORE_TRACE(e);
+		}
+
 		while (true);
 	}
 }
--- GammaEngine/src/GammaEngine/Application.h
+++ GammaEngine/src/GammaEngine/Application.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Core.h"
+#include "Events/Event.h"
+
 namespace GammaEngine {
 
 	class GAMMAENGINE_API Application
