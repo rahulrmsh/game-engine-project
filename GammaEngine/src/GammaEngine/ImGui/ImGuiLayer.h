478260b3eee0cf4098a09b40a1e71ebd22227cb9
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-05-08 12:56:01 +0530

    [Rahul] | Add. Events to ImGui Layers

--- GammaEngine/src/GammaEngine/ImGui/ImGuiLayer.h
+++ GammaEngine/src/GammaEngine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "GammaEngine/Layer.h"
+#include "GammaEngine/Events/ApplicationEvent.h"
+#include "GammaEngine/Events/KeyEvent.h"
+#include "GammaEngine/Events/MouseEvent.h"
 
 namespace GammaEngine {
 
@@ -14,6 +17,15 @@ namespace GammaEngine {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
