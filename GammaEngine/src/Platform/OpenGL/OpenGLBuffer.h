a5ee8c604753f3b8bfd9ba0e6d6e369ab0546fe5
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-05-17 22:14:15 +0530

    [Rahul] | Add. Buffer Layout Descriptors For Vertex Buffers

--- GammaEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ GammaEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -9,11 +9,14 @@ namespace GammaEngine {
 	public:
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
