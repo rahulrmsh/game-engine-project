bbf0594cf6956406a3d86b6f90fb885352deedae
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-05-17 22:30:56 +0530

    [Rahul] | Fix. Macro Name

--- GammaEngine/src/GammaEngine/Log.h
+++ GammaEngine/src/GammaEngine/Log.h
@@ -26,14 +26,14 @@ namespace GammaEngine {
 #define GAMMAENGINE_CORE_INFO(...)   ::GammaEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define GAMMAENGINE_CORE_WARN(...)   ::GammaEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define GAMMAENGINE_CORE_ERROR(...)  ::GammaEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define GAMMAENGINE_CORE_critical(...)  ::GammaEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define GAMMAENGINE_CORE_CRITICAL(...)  ::GammaEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 //Client Log Macros
 #define GAMMAENGINE_CLIENT_TRACE(...)  ::GammaEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define GAMMAENGINE_CLIENT_INFO(...)   ::GammaEngine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define GAMMAENGINE_CLIENT_WARN(...)   ::GammaEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define GAMMAENGINE_CLIENT_ERROR(...)  ::GammaEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GAMMAENGINE_CLIENT_critical(...)  ::GammaEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define GAMMAENGINE_CLIENT_CRITICAL(...)  ::GammaEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
 
 //If dist build
 #define GAMMAENGINE_CORE_INFO
