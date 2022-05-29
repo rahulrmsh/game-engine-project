2b1e00e306570b0646f10468ea6cd605fdf2b6ed
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-05-29 22:16:24 +0530

    [Rahul] | Refactor. Single Shader For Renderer2D

--- GammaEngine/src/GammaEngine/Renderer/Renderer2D.cpp
+++ GammaEngine/src/GammaEngine/Renderer/Renderer2D.cpp
@@ -11,7 +11,7 @@ namespace GammaEngine {
 	struct Renderer2DStorage
 	{
 		Ref<VertexArray> QuadVertexArray;
-		Ref<Shader> FlatColorShader;
+		Ref<Texture2D> WhiteTexture;
 		Ref<Shader> TextureShader;
 	};
 
@@ -42,7 +42,9 @@ namespace GammaEngine {
 		squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 		s_Data->QuadVertexArray->SetIndexBuffer(squareIB);
 
-		s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
+		s_Data->WhiteTexture = Texture2D::Create(1, 1);
+		uint32_t whiteTextureData = 0xffffffff;
+		s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 		s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetInt("u_Texture", 0);
@@ -55,8 +57,6 @@ namespace GammaEngine {
 
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
 
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
@@ -74,11 +74,11 @@ namespace GammaEngine {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetFloat4("u_Color", color);
+		s_Data->TextureShader->SetFloat4("u_Color", color);
+		s_Data->WhiteTexture->Bind();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
-		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
+		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
@@ -91,12 +91,12 @@ namespace GammaEngine {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
 	{
-		s_Data->TextureShader->Bind();
+		s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(1.0f));
+		texture->Bind();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
-		texture->Bind();
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
--- GammaEngine/src/Platform/OpenGL/OpenGLTexture.h
+++ GammaEngine/src/Platform/OpenGL/OpenGLTexture.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "GammaEngine/Renderer/Texture.h"
+#include <glad/glad.h>
 
 namespace GammaEngine {
 
@@ -9,15 +10,16 @@ namespace GammaEngine {
 	public:
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
-
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		virtual uint32_t GetWidth() const override { return m_Width; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
-
+		virtual void SetData(void* data, uint32_t size) override;
 		virtual void Bind(uint32_t slot = 0) const override;
 	private:
 		std::string m_Path;
 		uint32_t m_Width, m_Height;
 		uint32_t m_RendererID;
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 
 }
