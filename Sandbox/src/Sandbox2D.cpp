0cd5cd273f66916346b11587178d4559c02d22b5
Rahul Ramesh <rahulramesh13.rr@gmail.com>
2022-06-02 13:55:08 +0530

    [Ullas] | Add. Batch Rotated Textures/Quads

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -12,6 +12,7 @@ Sandbox2D::Sandbox2D()
 void Sandbox2D::OnAttach()
 {
 	GAMMAENGINE_PROFILE_FUNCTION();
+
 	m_CheckerboardTexture = GammaEngine::Texture2D::Create("assets/textures/Checkerboard.png");
 }
 
@@ -24,7 +25,9 @@ void Sandbox2D::OnUpdate(GammaEngine::Timestep ts)
 {
 	GAMMAENGINE_PROFILE_FUNCTION();
 
+	// Update
 	m_CameraController.OnUpdate(ts);
+
 	// Render
 	{
 		GAMMAENGINE_PROFILE_SCOPE("Renderer Prep");
@@ -33,13 +36,16 @@ void Sandbox2D::OnUpdate(GammaEngine::Timestep ts)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		GAMMAENGINE_PROFILE_SCOPE("Renderer Draw");
 		GammaEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		//GammaEngine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
-		GammaEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f },  { 0.8f, 0.2f, 0.3f, 1.0f });
+		GammaEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
+		GammaEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		GammaEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		GammaEngine::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		GammaEngine::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
+		GammaEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		GammaEngine::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
 		GammaEngine::Renderer2D::EndScene();
 	}
 }
